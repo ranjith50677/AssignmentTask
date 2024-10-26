57772db54da9421d84005c8b4c313e49c72db37d
RanjithKumar k <91112010+ranjith50677@users.noreply.github.com>
2024-10-26 16:22:09 +0530

    Update router.js

--- tasksever/view/router.js
+++ tasksever/view/router.js
@@ -1,14 +1,14 @@
 import express from "express";
-import { getAllUsers, getUserById, login, profile, reg, UpdateUser } from "../controller/usercontrol.js";
+import { getAllUsers, getUserById, login, ownerreg, profile, reg, UpdateUser } from "../controller/usercontrol.js";
 import { protect } from "../auth/index.js";
-
 const router=express.Router()
 
 router.post("/create",reg)
+router.post("/ownercreate",ownerreg)
 router.post("/login",login)
-router.get("/profile",protect, profile)
+router.get("/profile",protect,profile)
 router.get("/getalluser",getAllUsers)
 router.get("/getuser/:id",getUserById)
 router.put("/update/:id",UpdateUser)
 
-export default router; 
+export default router;
